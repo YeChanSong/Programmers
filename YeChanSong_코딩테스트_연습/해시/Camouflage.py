c1ca1edb186d0aa55c1d2084dbf76e80473f73bb
YeChanSong <davidsyc1170@gmail.com>
2020-08-13 17:10:01 +0900

    오답 수정중

--- YeChanSong_코딩테스트_연습/해시/Camouflage.py
+++ YeChanSong_코딩테스트_연습/해시/Camouflage.py
@@ -7,13 +7,26 @@ def solution(clothes):
     for i in clothes:
         dc[i[1]]+=1
     
-    for i in range(len(dc)):
-        li = list(map(list,combinations(dc.keys(),i+1)))
-        
+    for i in range(len(dc)-1):
+        if i == 0:
+            li = []
+            for k in dc.keys():
+                temp = [k]
+                li.append(temp)
+            #print(li)
+        else:
+            li = combinations(dc.keys(),i+1)
+            #print(li)
         for k in li:
             tmp2 = 1
             for t in k:
                 tmp2 *= dc[t]
             answer += tmp2
+    
+    tmp = 1
+    for i,v in dc.items():
+        
+        tmp *= v
+    answer += tmp
     
     return answer
